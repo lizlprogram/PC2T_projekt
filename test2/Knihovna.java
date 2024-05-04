aa4caecd7fb3ecb79f57b59afd7cfadf4fdf04f2
Fidler <dominik.fidler@siemens.com>
2024-05-04 13:53:46 +0200

    zprovoznit dbz s sql

--- test2/Knihovna.java
+++
@@ -1,9 +0,0 @@
-package projekt;
-
-/* public interface Knihovna {
-    void addKniha(Kniha Kniha);
-    void borrowKniha(Kniha Kniha);
-    void returnKniha(Kniha Kniha);
-    void displaydostupnostKnihas();
-    void displayKnihaDetails(Kniha Kniha);
-} */
